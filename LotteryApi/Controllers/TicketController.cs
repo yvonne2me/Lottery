1dff06ab3022d13bbbf151dffd26c582396a33b2
Yvonne <yvonne2me@gmail.com>
2021-09-27 15:58:25 +0100

    Initial commit to setup structure

---
+++ LotteryApi/Controllers/TicketController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Lottery.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketController : ControllerBase
+    {
+        private readonly ILogger<TicketController> _logger;
+
+        public TicketController(ILogger<TicketController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Get(Guid classId)
+        {
+            return Ok("OK");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Lottery API
+
+
+## Pre-requisites ##
+
+[.NET 5 SDK](https://dotnet.microsoft.com/download) is required to run this project.
+
+### How to use ###
+
+To run this project, navigate to the LotteryApi folder and run:
+
+    dotnet run
+
+
+To run all the unit tests for this project, navigate to LotteryApiTests folder and run:
+
+    dotnet test
